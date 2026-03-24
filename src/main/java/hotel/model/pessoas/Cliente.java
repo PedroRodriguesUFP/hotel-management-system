6906f58d67210168282c895d0b83070dfbe380a6
PedroRodrigues <pedrojoseabreurodrigues@gmail.com>
2026-03-24 22:31:47 +0000

    estrutura inicial

---
+++ src/main/java/hotel/model/pessoas/Cliente.java
@@ -0,0 +1,30 @@
+package hotel.model.pessoas;
+
+public class Cliente extends Pessoa {
+
+    private String nif;
+    private String nacionalidade;
+
+    public Cliente(String nome, String email, String telefone,
+                   String nif, String nacionalidade) {
+        super(nome, email, telefone);
+        this.nif = nif;
+        this.nacionalidade = nacionalidade;
+    }
+
+    // Getters
+    public String getNif() { return nif; }
+    public String getNacionalidade() { return nacionalidade; }
+
+    // Setters
+    public void setNif(String nif) { this.nif = nif; }
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | NIF: " + nif +
+                " | Nacionalidade: " + nacionalidade;
+    }
+}
---
+++ src/main/java/hotel/model/pessoas/Pessoa.java
@@ -0,0 +1,29 @@
+package hotel.model.pessoas;
+
+public abstract class Pessoa {
+
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Pessoa(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    // Getters
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public String getTelefone() { return telefone; }
+
+    // Setters
+    public void setNome(String nome) { this.nome = nome; }
+    public void setEmail(String email) { this.email = email; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + " | Email: " + email + " | Telefone: " + telefone;
+    }
+}
